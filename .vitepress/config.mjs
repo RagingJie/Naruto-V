c1e98e8969d572123a87c28c6b7d116462872146
Naruto <2045933252@qq.com>
2024-05-12 23:54:00 +0800

    提交

---
+++ .vitepress/config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/Naruto-V/',
+  title: "《Naruto-V》个性博客",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      {
+        text: '前端',
+        items: [
+          { text: 'VitePress中文官网', link: 'https://vitepress.qzxdp.cn' },
+          { text: 'vue', link: 'https://www.baidu.com' }
+        ]
+      },
+      { 
+        text: '后端',
+        items: [
+          {text: 'Java', link: '/Java-Study-Notes'},
+          {text: 'Python', link: '/Python-Study-Notes'}
+        ]
+      }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }, {
+        text: 'Java',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/RagingJie' }
+    ],
+
+    footer: {
+      message: '未曾放弃，何必认输，永觉不累',
+      copyright: 'Copyright © 2024-present Naruto-V'
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "《Naruto-V》"
+  text: "专注于技术能力的提升"
+  tagline: 若是生活没有了努力的方向，那活着的意义何在？
+  actions:
+    - theme: brand
+      text: 创建博客
+      link: /create-bolg
+    - theme: alt
+      text: 提升自我
+      link: /api-examples
+
+features:
+  - title: AAAA
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: BBBB
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: CCCC
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
