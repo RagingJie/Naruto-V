bdff55e955d22351e874363dc4fa02c43ce1b1f0
Naruto <2045933252@qq.com>
2024-05-13 14:22:54 +0800

    修改

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/Naruto-V/',
+  base: "/Naruto-V/",
   title: "《Naruto-V》个性博客",
   description: "A VitePress Site",
   themeConfig: {
